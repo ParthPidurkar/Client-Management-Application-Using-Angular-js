e141ca5d56f6dd07aa6869cea515ed31fd1e2940
Parth Pidurkar <85994476+ParthPidurkar@users.noreply.github.com>
2024-09-28 04:52:22 +0530

    Delete client-management-app directory

--- client-management-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ClientsComponent } from './components.css/clients/clients.component';
-import { MeetingsComponent } from './components.css/meetings/meetings.component';
-
-export const routes: Routes = [
-    { path: 'clients', component: ClientsComponent },
-    { path: 'meetings', component: MeetingsComponent },
-    { path: '', redirectTo: '/clients', pathMatch: 'full' }
-];
